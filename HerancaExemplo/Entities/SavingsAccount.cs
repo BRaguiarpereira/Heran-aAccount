0000c2f8376154800ce1abc8200ab6cb6d9a7004
Brian aguiar <briantbini@outlook.com>
2021-05-03 09:39:43 -0300

    Conceito de casting e downcasting

---
+++ HerancaExemplo/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaExemplo.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double loanlimit,double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalnce()
+        {
+            Balance += Balance * InterestRate;
+        }
+        
+    }
+}
